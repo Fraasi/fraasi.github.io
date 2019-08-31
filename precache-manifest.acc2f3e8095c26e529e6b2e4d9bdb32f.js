0cb36fe04b15099759a18d5cac1af89192d033fb
Fraasi <doofassi@gmail.com>
2019-08-31 23:57:22 +0300

    Deployed 8/31/2019, 11:57:05 PM

---
+++ precache-manifest.acc2f3e8095c26e529e6b2e4d9bdb32f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46b2deb48b484ac79b0560933b873f8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4df4c6cbed2723d7bf3",
+    "url": "/static/css/main.1992260f.chunk.css"
+  },
+  {
+    "revision": "c236cc98e6a6fc736206",
+    "url": "/static/js/2.e56b99db.chunk.js"
+  },
+  {
+    "revision": "e4df4c6cbed2723d7bf3",
+    "url": "/static/js/main.022e5dd1.chunk.js"
+  },
+  {
+    "revision": "39033e6823ea2a857138",
+    "url": "/static/js/runtime~main.e828fa91.js"
+  },
+  {
+    "revision": "4267d8aa8711bb8c72cbefb26066c9e0",
+    "url": "/static/media/BOOKOS.4267d8aa.TTF"
+  },
+  {
+    "revision": "d01731eecbeb065d79d807dacb623a93",
+    "url": "/static/media/black-linen.d01731ee.png"
+  },
+  {
+    "revision": "23a2f1cd41954b8808f9ee618b6cc1e0",
+    "url": "/static/media/code.23a2f1cd.svg"
+  },
+  {
+    "revision": "d059232c8762e199199af3c8772918c9",
+    "url": "/static/media/spinner.d059232c.svg"
+  },
+  {
+    "revision": "7fb66dd0e1b708790f14399636de2f8f",
+    "url": "/static/media/wenzel1.7fb66dd0.ttf"
+  }
+]);
